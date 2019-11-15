113b8fd3c140dc7964d238885e5f254806f508f4
ECAllen <ethancallen@gmail.com>
2019-11-15 09:46:06 -0500

    up to page 295

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,6 +15,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type contextKey string
+
+const contextKeyIsAuthenticated = contextKey("isAuthenticated")
+
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
